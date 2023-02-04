19f9285d71864f556793e8df707f3b4e9e8d45f1
mza <56841822+volcomsys@users.noreply.github.com>
2023-02-04 01:22:22 +0100

    green

--- scripts/buscador.js
+++ scripts/buscador.js
@@ -73,6 +73,8 @@ function doSearch() {
 
     } else if (total) {
 
+        lastTR.classList.add("green");
+
         td.innerHTML = "Se ha encontrado " + total + " coincidencia" + ((total > 1) ? "s" : "");
 
     } else {
